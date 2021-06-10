ab0291d907d9d68280f353cbdb77f0f92c0d46b3
Tatenda <tatenda@pop-os.localdomain>
2021-06-10 16:46:46 +0200

    added vpc

--- guru_tf_stack/main.tf
+++ guru_tf_stack/main.tf
@@ -32,6 +32,21 @@ resource "aws_db_instance" "example" {
   publicly_accessible  = true
 }
 
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "3.7"
+#     }
+#   }
+# }
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# resource "aws_vpc" "challenge" {
+#   cidr_block = "10.0.0.0/16"
 
 # provider "aws" {
 #   alias  = "us-east-1"
