3f90b4a281d7002a32bd551fcfcceb9c9e7d994c
Tatenda <tatenda@pop-os.localdomain>
2021-06-11 14:58:39 +0200

    added some useful commands

---
+++ commands.sh
@@ -0,0 +1,24 @@
+sudo apt-get install libssl1.0.0/trusty libssl-dev/trusty openssl/trusty
+
+#install python dependencies
+pip install flask configparser psycogy2 
+
+1. retrive rds endpoint name
+
+2 export hostname  # export PGHOST = (endpoint)
+ "echo $PGHOST)
+
+#Ensure you can connect to the database with the command below and enter in the bastion password:<br>
+3. psql -U challenge -h $PGHOST
+
+#Quit with \q
+
+Install Postgresql client and enable post 5432 in the security group else postgres client won't install
+#sudo apt -y install postgresql-client
+
+#Install the SQL files for the application
+4.psql -U challenge -h $PGHOST -f install.sql
+
+
+#Specify the Python version for the application
+5.python3 app.py
